cc73b5e127726761234390631d55dfd4d83a7e1f
Gema R. <gemagenoma@gmail.com>
2026-07-21 16:22:10 +0200

    Replace Galapagos with Yuyao and add new locations

--- script.js
+++ script.js
@@ -28,8 +28,10 @@ const locations = [
     { date: new Date(1690,4,2,8,2,34,52), name: 'Méjico', lat: 19.4326, lon: -99.1332, label: 4, color: accentColor },
     { date: new Date(1898,5,1,7,10,53,24), name: 'Paris', lat: 48.8566, lon: 2.3522, label: 5, color: accentColor },
 	{ date: new Date(1769,6,13,10,32,21,3), name: 'Tahiti', lat: -17.5028, lon: -149.4931, label: 6, color: accentColor },
-	{ date: new Date(-4280000000, 1, 2, 23, 56, 32, 341), name: 'Galapagos', lat: -21.1815, lon: -109.0718, label: 7, color: accentColor },
-	{ date: new Date(-259000, 10, 7, 4, 3, 6, 54), name: 'Florisbad', lat: -28.7660, lon: 26.0830, label: 8, color: accentColor }
+	{ date: new Date(-5000, 1, 2, 23, 56, 32, 341), name: 'Yuyao', lat: 29.9642, lon: 121.3444, label: 7, color: accentColor },
+	{ date: new Date(-259000, 10, 7, 4, 3, 6, 54), name: 'Florisbad', lat: -28.7660, lon: 26.0830, label: 8, color: accentColor },
+	{ date: new Date(1520, 11, 28, 12, 4, 1, 43), name: 'Estrecho de Magallanes', lat: -28.7660, lon: 26.0830, label: 9, color: accentColor },
+	{ date: new Date(1946, 5, 1, 8, 24, 21, 98), name: 'Pilbara', lat: -20.3107, lon: 118.5753, label: 10, color: accentColor }
 ];
 
 // Convert lat/lon to 3D coordinates on sphere
